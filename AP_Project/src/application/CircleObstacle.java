74e8636d326fd8a9b088ec1f6744fa34e7c9ffb9
Krishna Nimmagadda <55681302+nkrishnasatvik@users.noreply.github.com>
2020-11-21 11:33:24 +0400

    Add files via upload

---
+++ AP_Project/src/application/CircleObstacle.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class CircleObstacle extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+		Circle out=new Circle();
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ AP_Project/src/application/MainPage.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.*;
+
+public class MainPage extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+}
