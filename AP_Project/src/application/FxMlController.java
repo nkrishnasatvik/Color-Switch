1c6c952cdfc9852fcda9e2115e4115743315f1ad
PercyAbhi <68178959+PercyAbhi@users.noreply.github.com>
2020-11-22 16:47:43 +0530

    Add files via upload

---
+++ AP_Project/src/application/FxMlController.java
@@ -0,0 +1,42 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.layout.AnchorPane;
+
+public class FxMlController {
+
+    @FXML
+    private AnchorPane MainPage;
+
+    @FXML
+    private AnchorPane ObstacleSquare;
+
+    @FXML
+    private Group Square;
+
+    @FXML
+    private AnchorPane ObstacleTri;
+
+    @FXML
+    private Group Triangle;
+
+    @FXML
+    private AnchorPane ObstaclePlus;
+
+    @FXML
+    private Group Plus;
+
+    @FXML
+    private AnchorPane SavedMenu;
+
+    @FXML
+    private AnchorPane PausedScreen;
+
+    @FXML
+    private AnchorPane ObstacleCircle;
+
+    @FXML
+    private Group Circle;
+
+}
