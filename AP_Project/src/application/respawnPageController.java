4d5c5dd75c066bae46a70c4b8364fbd2db76debc
PercyAbhi <68178959+PercyAbhi@users.noreply.github.com>
2020-12-16 21:26:34 +0530

    Update respawnPageController.java
    Updating the score and highscore

--- AP_Project/src/application/respawnPageController.java
+++ AP_Project/src/application/respawnPageController.java
@@ -1,10 +1,20 @@
 package application;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+
 import javafx.scene.control.Label;
+
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class respawnPageController {
 
@@ -28,15 +38,55 @@ public class respawnPageController {
 
     @FXML
     private Button restartButton;
-
-    @FXML
-    void resumeBTAction(ActionEvent event) {
+    
+    private static int noOfStars;
+	private static int highScoreVal;
+	private int scoreVal;
+    
+    // Event Listener on Button[#mainMenuButton].onAction
+	@FXML
+	public void mainMenuBTAction(ActionEvent event) throws IOException {
+		
+		Parent GamePageParent = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+    	Scene GamePageScene = new Scene(GamePageParent);
     	
-    }
-
-    @FXML
-    void saveBTAction(ActionEvent event) {
-
-    }
+    	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	window.setScene(GamePageScene);
+    	window.show();
+    	
+	}
+	
+	// Event Listener on Button[#respawnButton].onAction
+	@FXML
+	public void respawnBTAction(ActionEvent event) throws IOException {
+		
+	}
+	
+	// Event Listener on Button[#restartButton].onAction
+	@FXML
+	public void restartBTAction(ActionEvent event) throws IOException {
+		
+		Parent GamePageParent = FXMLLoader.load(getClass().getResource("GamePage.fxml"));
+    	Scene GamePageScene = new Scene(GamePageParent);
+    	
+    	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	window.setScene(GamePageScene);
+    	window.show();
+    	
+	}
+	
+	public void setLabels(int gamescore) {
+		
+		this.score.setText(Integer.toString(gamescore));
+		
+		noOfStars += gamescore;
+		this.totalStars.setText(Integer.toString(noOfStars));
+		
+		if(highScoreVal < gamescore) {
+			highScoreVal = gamescore;
+			this.highScore.setText(Integer.toString(highScoreVal));
+		}
+		
+	}
 
 }
