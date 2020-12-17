1fe3e26d1edda15a020aff42bc36f6e1ee77e263
Krishna Nimmagadda <55681302+nkrishnasatvik@users.noreply.github.com>
2020-12-17 10:14:58 +0400

    Update RespawnPageController.java

--- AP_Project/src/application/RespawnPageController.java
+++ AP_Project/src/application/RespawnPageController.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -9,77 +10,71 @@ import javafx.scene.control.Button;
 
 import java.io.IOException;
 
-import javafx.event.ActionEvent;
-
 import javafx.scene.control.Label;
 
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class RespawnPageController {
-	@FXML
-	private AnchorPane PausedScreen;
-	@FXML
-	private Button mainMenuButton;
-	@FXML
-	private Button respawnButton;
-	@FXML
-	private Label totalStars;
-	@FXML
-	private Label score;
-	@FXML
-	private Label highScore;
-	@FXML
-	private Button restartButton;
-	
-	private static int noOfStars;
-	private static int highScoreVal;
-	private int scoreVal;
-	
+public class respawnPageController {
 
-	// Event Listener on Button[#mainMenuButton].onAction
+    @FXML
+    private AnchorPane PausedScreen;
+    @FXML
+    private Button mainMenuButton;
+    @FXML
+    private Button respawnButton;
+    @FXML
+    private Label totalStars;
+    @FXML
+    private Label score;
+    @FXML
+    private Label highScore;
+
+    @FXML
+    private Button restartButton;
+
 	@FXML
 	public void mainMenuBTAction(ActionEvent event) throws IOException {
-		
+
 		Parent GamePageParent = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
-    	Scene GamePageScene = new Scene(GamePageParent);
-    	
-    	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-    	window.setScene(GamePageScene);
+    	Main.scene.setRoot(GamePageParent);
+
+    	Stage window = (Stage)Main.scene.getWindow();
+    	window.setScene(Main.scene);
     	window.show();
-    	
+
 	}
 	
-	// Event Listener on Button[#respawnButton].onAction
 	@FXML
 	public void respawnBTAction(ActionEvent event) throws IOException {
-		
+
 	}
-	
-	// Event Listener on Button[#restartButton].onAction
+
 	@FXML
 	public void restartBTAction(ActionEvent event) throws IOException {
-		
+
 		Parent GamePageParent = FXMLLoader.load(getClass().getResource("GamePage.fxml"));
-    	Scene GamePageScene = new Scene(GamePageParent);
-    	
-    	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-    	window.setScene(GamePageScene);
+    	Main.scene.setRoot(GamePageParent);
+
+    	Stage window = (Stage)Main.scene.getWindow();
+    	window.setScene(Main.scene);
     	window.show();
-    	
+
 	}
-	
+
 	public void setLabels(int gamescore) {
-		
+
 		this.score.setText(Integer.toString(gamescore));
 		
-		noOfStars += gamescore;
-		this.totalStars.setText(Integer.toString(noOfStars));
-		
-		if(highScoreVal < gamescore) {
-			highScoreVal = gamescore;
-			this.highScore.setText(Integer.toString(highScoreVal));
+		Main.allGames.setTotalStars(Main.allGames.getTotalStars()+gamescore);
+		this.totalStars.setText(Integer.toString(Main.allGames.getTotalStars()));
+
+		if(Main.allGames.getHighScore() < gamescore) {
+			Main.allGames.setHighScore(gamescore);
 		}
 		
+		this.highScore.setText(Integer.toString(Main.allGames.getHighScore()));
+
 	}
+
 }
